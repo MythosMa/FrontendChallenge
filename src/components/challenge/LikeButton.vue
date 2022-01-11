083945e120bb217d5948d9838566379003cb1e2f
MythosMa <mythosmayuan@gmail.com>
2022-01-11 15:28:00 +0800

    udpate

--- src/components/challenge/LikeButton.vue
+++ src/components/challenge/LikeButton.vue
@@ -32,3 +32,51 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  @extend .is-flex-column-center;
+  width: 100%;
+  height: 100%;
+  background-color: aqua;
+
+  .heart {
+    position: relative;
+    transform: rotate(-45deg);
+
+    .heart-left {
+      transform: translate(-50%, -100%);
+    }
+
+    .heart-right {
+      transform: translate(0, -50%);
+    }
+
+    .heart-square {
+      transform: translate(-50%, -50%);
+    }
+
+    .heart-left,
+    .heart-right {
+      border-radius: 50%;
+    }
+
+    .heart-left,
+    .heart-right,
+    .heart-square {
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      background-color: grey;
+    }
+  }
+
+  .heart-is-click {
+    .heart-left,
+    .heart-right,
+    .heart-square {
+      background-color: red;
+    }
+  }
+}
+</style>
